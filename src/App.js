406e9326f908ad0d56f32756036f1bf9bfe03f68
Ben Li <ben.li@qantas.com.au>
2019-04-11 11:18:48 +1000

    fetch data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  state = {
+    coins: []
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    const { coins } = this.state; 
     return (
-      <div className="App">
-        <header className="App-header">
-            Learn React
-        </header>
-      </div>
+      <>
+        <h3>coins:</h3>
+        {
+          coins.map(coin => (
+            <div className='coin-container' key={coin.id}>
+              <div className="name">{coin.name}</div>
+              <div className="price">{coin.price_usd}</div>
+            </div>
+          ))
+        }
+      </>
     );
+  }
+
+  fetchData() {
+    return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10')
+          .then(response => {
+            if(!response.ok) {
+              throw new Error('Failed to fetch');
+            }
+            return response.json();
+          })
+          .then(coins => {
+            this.setState({ coins })
+          })
   }
 }
 
